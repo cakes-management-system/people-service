d9eee35ce1619e91ed7f7b5e022bb4ee1bfe866b
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2022-02-09 19:38:35 +0300

    Enable feign client

--- src/main/java/com/koltsov/cms/service/people/PeopleServiceApplication.java
+++ src/main/java/com/koltsov/cms/service/people/PeopleServiceApplication.java
@@ -2,7 +2,9 @@ package com.koltsov.cms.service.people;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PeopleServiceApplication {
 
